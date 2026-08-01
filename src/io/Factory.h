b7649a34be4380d54b7a3932923896dfb5c04a7f
Ferdinand <atiferdinand31@gmail.com>
2026-08-01 17:26:03 +0100

    feat(io): implement Factory class who produce an Source provider.

---
+++ src/io/Factory.h
@@ -0,0 +1,30 @@
+//
+// Created by ferdinand on 8/1/26.
+//
+
+#ifndef FEROXILLS_FACTORY_H
+#define FEROXILLS_FACTORY_H
+
+#include  "ISampleProvider.h"
+#include  "SoftwareProvider.h"
+
+class ProviderFactory {
+    public:
+       static ISampleProvider * createProvider(const ProviderType type) {
+           switch (type) {
+               case ProviderType::SOFTWARE_SOURCE:
+                   return  new SoftwareProvider();
+               case ProviderType::USB_SOURCE:
+                   break;
+               case ProviderType::UART_SOURCE:
+                   break;
+               case ProviderType::NO_SOURCE:
+                   break;
+               default:
+                   break;
+           }
+           return  nullptr;
+       }
+};
+
+#endif //FEROXILLS_FACTORY_H
